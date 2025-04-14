9f9621c06cc895698c6391b653c3467ef4b7c02c
truds99 <dudscardoso9@gmail.com>
2025-04-14 17:02:54 -0300

    add customer repository

---
+++ src/main/java/com/boardcamp/api/repositories/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.boardcamp.api.repositories;
+
+import com.boardcamp.api.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByCpf(String cpf);
+}
